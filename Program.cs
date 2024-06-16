e5f36d0bd80b94a1c3502dde45ebbb10146b1a46
neufrankie <neu.vietanh272@gmail.com>
2024-06-16 19:56:26 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexOutOfRangeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6]; 
+                Console.WriteLine($"Gia tri cua phan tu thu 6 la: {value}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Da xay ra loi: " + ex.Message);
+            }
+            Console.WriteLine("Chuong trinh da ket thuc.");
+        }
+    }
+}
